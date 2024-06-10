41279ffa1426ef0759ae07e54e6acdc3654e90b0
jaguifon1 <9001374@alumnos.ufv.es>
2024-06-10 19:17:52 +0200

    Estrucutra principal Primer commit

---
+++ Back/src/test/java/com/es/ufv/dis/ordinaria/api/JAF/JafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.ufv.dis.ordinaria.api.JAF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
